11614162656f11c1ecaf7cc782b914254d6cdba7
viet-quoctran <67690248+viet-quoctran@users.noreply.github.com>
2024-04-29 16:50:24 +0700

    Update main.py
    edit

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import StaleElementReferenceException, TimeoutException
 import time
-print(123)
+
 python_files = ["clear_data.py"]
 def click_safe(driver, xpath):
     try:
+        print(111)
         element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, xpath)))
         element.click()
     except (StaleElementReferenceException, TimeoutException) as e:
