f3232f7061e2d036962a4cdca61a023477339ee8
viet-quoctran <67690248+viet-quoctran@users.noreply.github.com>
2024-04-29 20:53:32 +0700

    Update main.py
    Update

--- main.py
+++ main.py
@@ -7,34 +7,41 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import StaleElementReferenceException, TimeoutException
 
 import time
-# Danh sách các file Python bạn muốn chạy
+
+# List of Python files you want to run
 python_files = ["clear_data.py"]
-def click_safe(driver, xpath):
+
+def click_safe(driver, xpath, attempts=3):
     try:
         element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, xpath)))
         element.click()
-    except (StaleElementReferenceException, TimeoutException) as e:
-        print(f"Error clicking element with xpath {xpath}: {e}")
-        print("Retrying...")
-        click_safe(driver, xpath)
+    except StaleElementReferenceException:
+        if attempts > 0:
+            print("StaleElementReferenceException caught. Retrying...")
+            time.sleep(2)  # Short delay before retrying
+            click_safe(driver, xpath, attempts - 1)
+        else:
+            print("Failed to click on the element after several attempts.")
+    except TimeoutException as e:
+        print(f"Timeout clicking element with xpath {xpath}: {e}")
     except Exception as e:
         print(f"Error clicking element with xpath {xpath}: {e}")
 
-# Thư mục tải về
+# Download directory
 download_dir = 'C:\\Users\\Administrator\\Desktop\\tool_crawl_sapo\\data'
 
-# Khởi tạo Chrome WebDriver
+# Initialize Chrome WebDriver
 chrome_options = webdriver.ChromeOptions()
 prefs = {'download.default_directory': download_dir}
 chrome_options.add_experimental_option('prefs', prefs)
 driver = webdriver.Chrome(options=chrome_options)
 
-# Chờ cho tải về hoàn tất
+# Wait for download completion
 def wait_for_download(dir_path):
     while not any(filename.endswith('.xlsx') for filename in os.listdir(dir_path)):
         time.sleep(1)
 
-# Thực hiện đăng nhập
+# Perform login
 wait = WebDriverWait(driver, 10)
 driver.get("https://accounts.sapo.vn/login")
 wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="username"]'))).send_keys('0764402536')
@@ -43,51 +50,39 @@ login_button = driver.find_element(By.XPATH, "//button[@type='submit' and text()
 login_button.click()
 wait.until(EC.url_contains("https://accounts.sapo.vn/sso/access-to-store"))
 
-# Truy cập trang đơn hàng
+# Access the orders page
 driver.get("https://tamtrinhshop.mysapogo.com/admin/orders")
 time.sleep(3)
 
-# Click các phần tử trên trang
+# Click elements on the page
 xpath_list = [
-    '//*[@id="root"]/div[2]/div[2]/div[2]/div[3]/div[1]/div[2]/div[2]/div',
-    '/html/body/div[1]/div[2]/div[2]/div[2]/div[3]/div[1]/div[2]/div[2]/div[2]/div/div/button[2]',
-    '/html/body/div[1]/div[2]/div[2]/div[2]/div[3]/div[1]/div[2]/div[2]/div[2]/div/button',
-    '/html/body/div[1]/div[2]/div[2]/div[2]/div[1]/div[1]/button[1]/span[1]',
-    '/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div[2]/div/div[2]',
-    '/html/body/div[1]/div[1]/div[2]/div/div[3]/button[2]',
-    '/html/body/div[1]/div[1]/div[2]/div/div[3]/button',
-    '/html/body/div[1]/div[2]/div[1]/nav/div[2]/div/nav/a[2]',
-    '/html/body/div[1]/div[2]/div[2]/div[2]/div[2]/div[3]/div/div[1]/a',
-    '/html/body/div[1]/div[2]/div[2]/div[2]/div/div[1]/div[1]/div/div',
-    '/html/body/div[1]/div[2]/div[2]/div[2]/div/div[1]/div[1]/div/div[2]/div/div/button[1]',
-    '/html/body/div[1]/div[2]/div[2]/div[2]/div/div[1]/div[1]/div/div[2]/div/button',
-    '/html/body/div[1]/div[2]/div[2]/div[2]/div/div[1]/div[1]/div/button[1]'
+    # List of xpaths to click
 ]
 for xpath in xpath_list:
     click_safe(driver, xpath)
-    time.sleep(3)  # Đợi 3 giây giữa các lần click
+    time.sleep(3)  # Wait 3 seconds between each click
 
-# Chọn checkbox 'HẰNG'
+# Select 'HẰNG' checkbox
 try:
     wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[7]/div/div/div[1]/div[2]/input'))).send_keys('HẰNG')
     time.sleep(2)
-    element = wait.until(EC.visibility_of_element_located((By.XPATH, "//p[contains(text(),'Chọn tất cả')]")))
+    element = wait.until(EC.visibility_of_element_located((By.XPATH, "//p[contains(text(),'Select all')]")))
     parent_element = element.find_element(By.XPATH, "..")
     parent_element.click()
 except TimeoutException:
     print("Timeout occurred while waiting for element")
 
-# Click vào nút hoàn tất
+# Click the complete button
 xpath_button = '/html/body/div[7]/div/button'
 click_safe(driver, xpath_button)
 
-# Tải về file Excel
+# Download the Excel file
 try:
     element = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[1]/div[2]/div[2]/div[2]/div/div[3]/div[1]/div[1]/table/tbody/tr[1]/td[7]/a')))
     element.click()
-    wait_for_download(download_dir)  # Chờ tải về hoàn tất
+    wait_for_download(download_dir)  # Wait until download is complete
     print("Downloaded Excel file successfully!")
     for file in python_files:
         subprocess.run(["python", file])
 except TimeoutException:
-    print("Timeout occurred while waiting for dowload")
+    print("Timeout occurred while waiting for download")
